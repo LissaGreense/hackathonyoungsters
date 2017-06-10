934541bde32169b6e4a8a675b02a4e242b647af0
Oskar Przystawko <Oskar.Przystawko@gmail.com>
2017-06-10 05:00:43 +0200

    Create class TramOrBus, modify popup xml

---
+++ Android_App/MyApplication2/app/src/main/java/com/youngsters/myapplication2/TramOrBusFragment.java
@@ -0,0 +1,27 @@
+package com.youngsters.myapplication2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Michał 1984 on 2017-06-10.
+ */
+
+public class TramOrBusFragment extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.popup, container, false);
+
+
+
+        return view;
+    }
+}
